ff6d5f13c24b0ad32b43516e8f4955dce285e3f6
nsfranklin <nickfranklin@rocketmail.com>
2019-02-14 09:49:57 +0000

    Progress

--- src/Player150402149.java
+++ src/Player150402149.java
@@ -2,7 +2,6 @@ import java.awt.*;
 
 public class Player150402149 extends GomokuPlayer{
     public final int MAXDEPTH = 3;
-
     public  final int[][][] leftToRightDiag = {{{0,3},{1,4},{2,5},{3,6},{4,7}},
             {{0,2},{1,3},{2,4},{3,5},{4,6},{5,7}},
             {{0,1},{1,2},{2,3},{3,4},{4,5},{5,6},{6,7}},
@@ -10,8 +9,6 @@ public class Player150402149 extends GomokuPlayer{
             {{1,0},{2,1},{3,2},{4,3},{5,4},{6,5},{7,6}},
             {{2,0},{3,1},{4,2},{5,3},{6,4},{7,5}},
             {{3,0},{4,1},{5,2},{6,3},{7,4}}};
-
-
     public final int[][][] rightToLeftDiag = {{{0,4},{1,3},{2,2},{3,1},{4,0}},
             {{0,5},{1,4},{2,3},{3,2},{4,1},{5,0}},
             {{0,6},{1,5},{2,4},{3,3},{4,2},{5,1},{6,0}},
@@ -19,7 +16,6 @@ public class Player150402149 extends GomokuPlayer{
             {{1,7},{2,6},{3,5},{4,4},{4,3},{5,2},{6,1}},
             {{2,7},{3,6},{4,5},{5,4},{6,3},{7,2}},
             {{3,7},{4,6},{5,5},{6,4},{7,3}}}; //read only therefore less memory used if global.
-
     public int[] DiagSize = {5, 6, 7, 8, 7, 6, 5};
 
 
@@ -29,50 +25,54 @@ public class Player150402149 extends GomokuPlayer{
     }
 
     public Integer[] AlphaBetaMaxPlayer(Color[][] state, int min, int max, int depth, Color player){
+        Color nextPlayerColor = nextPlayer(player);
 
-        if(CutOffTest(state, player)){
+        if(CutOffTest(state, player, depth)){
             Integer[] returned =  {null,null,utilityFunction(state)};
             return returned;
         }
-        int[] best = new int[3]; // [x,y,Utility]
-        for(){
-
-             if(){
-
-             }else if(){
-
-             }else if(){
+        Integer[] best = new Integer[3]; // [x,y,Utility]
+        int[] actionAndReturnedUtility = new int[3];
+        for(int i = 0 ; i < actionsAvailable.length; i++){
+            actionAndReturnedUtility = AlphaBetaMinPlayer(makeMove(state, actionsAvailable[i], nextPlayerColor), min , max , depth - 1 , nextPlayerColor );
+             if(actionAndReturnedUtility[2] > best[2]){
+                best = {};
+             }else if(actionAndReturnedUtility[2] >= ){
+                 return ;
+             }else if(actionAndReturnedUtility[2] > min){
 
              }
         }
-
         return best;
     }
 
     public Integer[] AlphaBetaMinPlayer(Color[][] state, int min, int max,int depth, Color player){
-
-        Integer[] best = new Integer[3]; // [x,y,Utility]
-
-        if(CutOffTest(state, player)){
+        Color nextPlayerColor = nextPlayer(player);
 
 
-            return ()
+        if(CutOffTest(state, player, depth)){
+            Integer[] returned =  {null,null,utilityFunction(state)};
+            return returned;
         }
-        for(){
+        Integer[] best = new Integer[3]; // [x,y,Utility]
+        int[] actionAndReturnedUtility = new int[3];
+        for(int i = 0 ; i < actionsAvailable.length; i++){
+            actionAndReturnedUtility = AlphaBetaMaxPlayer(makeMove(state, actionsAvailable[i], nextPlayerColor), min , max , depth - 1 , nextPlayerColor );
             if(){
 
+            }else if(){
+
+            }else if(){
+
             }
-
         }
-
         return best;
     }
 
-    public boolean CutOffTest(Color[][] state, Color player){  //terminal or max depth
+    public boolean CutOffTest(Color[][] state, Color player, int depth){  //terminal or max depth
 
         int count1 = 0;
         int count2 = 0;
-
         for(int i = 0 ; i < 8 ; i++ ){ //the verticle and horizontal
             for(int j = 0 ; j < 8 ; j++){
                 if(state[i][j].equals(player)){
@@ -116,6 +116,20 @@ public class Player150402149 extends GomokuPlayer{
 
     public int utilityFunction(Color[][] state){
 
+    }
+
+    public Color[][] makeMove(Color[][] state, int[] move, Color player){
+        Color[][] newState = state;
+        newState[move[0]][move[1]] = player;
+        return newState;
+    }
+
+    public Color nextPlayer(Color player){
+        if(player.equals(Color.black)) {
+            return Color.white;
+        }else{
+            return Color.black;
+        }
     }
     /*
     public boolean CutOffTestHighlyOptimised(Color[][] state, int[] best, int depth, Color player){  //terminal or max depth
